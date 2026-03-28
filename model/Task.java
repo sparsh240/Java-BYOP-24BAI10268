f6aff3d0232345da5fe9ed19fe9471144a848191
sparsh <mailsparshgupta682@gmail.com>
2026-03-28 20:07:59 +0530

    Adding Task and User

--- model/Task.java
+++ model/Task.java
@@ -1,5 +1,35 @@
 package model;
 
-public class Task {
-    
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task implements Serializable {
+    private static int counter = 1;
+
+    private int id;
+    private String title;
+    private String owner;
+    private List<String> sharedWith;
+    private boolean completed;
+
+    public Task(String title, String owner, String[] sharedUsers) {
+        this.id = counter++;
+        this.title = title;
+        this.owner = owner;
+        this.sharedWith = new ArrayList<>();
+        for (String user : sharedUsers) {
+            sharedWith.add(user.trim());
+        }
+        this.completed = false;
+    }
+
+    public int getId() { return id; }
+    public String getTitle() { return title; }
+    public String getOwner() { return owner; }
+    public List<String> getSharedWith() { return sharedWith; }
+    public boolean isCompleted() { return completed; }
+
+    public void markComplete() { this.completed = true; }
+    public void shareWith(String username) { sharedWith.add(username); }
 }
--- model/User.java
+++ model/User.java
@@ -1,5 +1,19 @@
 package model;
 
-public class User {
-    
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String username;
+    private String salt;
+    private String passwordHash;
+
+    public User(String username, String salt, String passwordHash) {
+        this.username = username;
+        this.salt = salt;
+        this.passwordHash = passwordHash;
+    }
+
+    public String getUsername() { return username; }
+    public String getSalt() { return salt; }
+    public String getPasswordHash() { return passwordHash; }
 }
