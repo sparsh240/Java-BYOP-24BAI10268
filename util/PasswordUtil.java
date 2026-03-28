50c68e786fc9f60d3ee1c3ab212cc1eac8b71aee
sparsh <mailsparshgupta682@gmail.com>
2026-03-28 20:12:17 +0530

    Security Handling

--- util/PasswordUtil.java
+++ util/PasswordUtil.java
@@ -1,5 +1,19 @@
 package util;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class PasswordUtil {
-    
+    public static String hashPassword(String password, String salt) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            String input = password + salt;
+            byte[] hashed = md.digest(input.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : hashed) sb.append(String.format("%02x", b));
+            return sb.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Hashing algorithm not found");
+        }
+    }
 }
--- util/SaltUtil.java
+++ util/SaltUtil.java
@@ -1,5 +1,16 @@
 package util;
 
+import java.security.SecureRandom;
+
 public class SaltUtil {
-    
+    private static final SecureRandom random = new SecureRandom();
+    private static final String CHARSET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    public static String generateSalt() {
+        StringBuilder sb = new StringBuilder(16);
+        for (int i = 0; i < 16; i++) {
+            sb.append(CHARSET.charAt(random.nextInt(CHARSET.length())));
+        }
+        return sb.toString();
+    }
 }
